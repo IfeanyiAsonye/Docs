71a75efb6ac4d6153c0f401407708e26731adbeb
Ifeanyi <Asonyejude@gmail.com>
2026-01-03 12:30:52 +0100

    "Initial commit of AuditScript.sh"

---
+++ AuditScript.sh
@@ -0,0 +1,25 @@
+```bash
+#!/bin/bash
+
+# Get system information 
+echo "Hostname: $(hostname)"
+echo "Os Version $(uname -r)"
+echo "System uptime $(uptime -p)"
+
+echo "------------------------------------------------"
+
+# User information
+echo "Logged in $(who)"
+
+echo "------------------------------------------------"
+
+# Network Security Checks
+echo "Firewall Status"
+sudo ufw status 
+
+echo "------------------------------------------------"
+
+# Network Status 
+netstat -antop 
+
+```
